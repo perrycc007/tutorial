1055be4b36d63a663ab76ecc44ab4f4cec940424
perrycc007 <77976566+perrycc007@users.noreply.github.com>
2022-12-03 16:32:13 +0000

    close button in tutor

--- components/Form/Forms/Budget.js
+++ components/Form/Forms/Budget.js
@@ -1,51 +1,84 @@
-import React from 'react';
-import { useState, useCallback } from 'react';
-import MinSlider from '../../InputTool/Slider';
-import Typography from '@mui/material/Typography';
+import React from "react";
+import { useState, useCallback } from "react";
+import MinSlider from "../../InputTool/Slider";
+import Typography from "@mui/material/Typography";
+import Axios from "axios";
 
+const Budget = (props) => {
+  // const [fee,setFee] = useState([])
+  const [status, setStatus] = useState(props.info.status);
+  const info = props.info;
+  const [userData, setUserData] = useState(info | initialUserData);
+  console.log(info);
+  async function toggleStatus(status) {
+    const response = await Axios.patch(
+      `http://localhost:3001/history/updateTutorStatus`,
+      {
+        tutorid: props.info.tutorid,
+        status: status,
+      }
+    );
+    response.data.result;
+  }
 
-const Budget = (props) =>{
-// const [fee,setFee] = useState([])
-const info = props.info
-const [userData, setUserData] = useState(info|initialUserData);
-
-const initialUserData = {
+  const StatusHandler = (event) => {
+    event.preventDefault();
+    if (status == "open") {
+      setStatus("close");
+      toggleStatus("close");
+    } else {
+      setStatus("open");
+      toggleStatus("open");
+    }
+  };
+  const initialUserData = {
     lowestfee: 60,
     highestfee: 1000,
   };
 
-// const FeeHandler = (value) => {
-//     setFee(value)
-// }
-const updateUserDataHandler = (value) => {
-        value[0] == null? value[0] = 60 : value[0]
-        value[1] == null? value[1] = 1000 : value[1]
-        console.log(value)
-        setUserData({ ...userData, lowestfee: value[0], highestfee: value[1]  });
-        console.log(userData)
-    }
-
-
-
-
-const formHandler = (event) => {
-    event.preventDefault()
-    console.log(userData)
-    props.submitHandler(userData)
+  // const FeeHandler = (value) => {
+  //     setFee(value)
+  // }
+  const updateUserDataHandler = (value) => {
+    value[0] == null ? (value[0] = 60) : value[0];
+    value[1] == null ? (value[1] = 1000) : value[1];
+    console.log(value);
+    setUserData({ ...userData, lowestfee: value[0], highestfee: value[1] });
+    console.log(userData);
   };
 
-    return(
-        <React.Fragment>
-            <form>
-                <Typography component={'span'} variant={'body2'} sx={{ width: '33%', flexShrink: 0 }}>
-                補習學費每小時
-                </Typography>
-            <MinSlider  step ={20} max={1000} min={60} dmax={info?info['highestfee']:100} dmin={info?info['lowestfee']:200}  minD={20} passValue={updateUserDataHandler}/>
+  const formHandler = (event) => {
+    event.preventDefault();
+    console.log(userData);
+    props.submitHandler(userData);
+  };
 
-                <button onClick={formHandler}>Save</button>
-                </form>
-        </React.Fragment>
-    )
-}
+  return (
+    <React.Fragment>
+      <form>
+        <Typography
+          component={"span"}
+          variant={"body2"}
+          sx={{ width: "33%", flexShrink: 0 }}
+        >
+          補習學費每小時
+        </Typography>
+        <MinSlider
+          step={20}
+          max={1000}
+          min={60}
+          dmax={info ? info["highestfee"] : 100}
+          dmin={info ? info["lowestfee"] : 200}
+          minD={20}
+          passValue={updateUserDataHandler}
+        />
+        <button onClick={StatusHandler}>
+          {status == "open" ? "Open" : "Close"}
+        </button>
+        <button onClick={formHandler}>Save</button>
+      </form>
+    </React.Fragment>
+  );
+};
 
 export default Budget;
