264ac2ba50f2044ea83ca12c41ff239feb6fa574
perrycc007 <77976566+perrycc007@users.noreply.github.com>
2023-01-26 18:20:34 +0000

    added loading screen for profile

--- pages/profile/[userid]/index.js
+++ pages/profile/[userid]/index.js
@@ -1,36 +1,47 @@
-import * as React from 'react';
-import Axios from 'axios';
-import ProfileForm from '../../../components/Form/ProfileForm';
+import Axios from "axios";
+import ProfileForm from "../../../components/Form/ProfileForm";
+import { useEffect, useState } from "react";
+import LoadingScreen from "../../../components/Layout/LoadingScreen";
 const UserProfile = (props) => {
+  const [loading, setLoading] = useState(true);
+  console.log(props.reponse);
+
+  useEffect(() => {
+    if (props.status == 200) {
+      setLoading(false);
+    }
+  }),
+    [props.reponse];
   return (
     <>
-      <ProfileForm  profile={props.profile} tutor={props.tutor}/>
+      {loading && <LoadingScreen />}
+      {!loading && <ProfileForm profile={props.reponse} tutor={props.tutor} />}
     </>
   );
 };
 export async function getStaticPaths() {
-    return { 
-      paths: []
-    , fallback: true }
-  }
-  
+  return {
+    paths: [],
+    fallback: true,
+  };
+}
+
 export async function getStaticProps(context) {
   const { params } = context;
   // fetch data from an API
-  const response = await Axios.get(`http://localhost:3001/profile/${params.userid}`,)
-  const tutor = await Axios.get(`http://localhost:3001/tutor/${params.userid}`,)
+  const response = await Axios.get(
+    `http://localhost:3001/profile/${params.userid}`
+  );
+  const tutor = await Axios.get(`http://localhost:3001/tutor/${params.userid}`);
 
-
-  
-  console.log(response.data.result)
-  console.log(tutor.data.result)
   return {
     props: {
-      profile: response.data.result,
+      reponse: response.data.result,
       tutor: tutor.data.result,
+      status: response.status,
     },
     revalidate: 1,
   };
-};
+}
 
 export default UserProfile;
